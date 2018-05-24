84ec8841a27d0c795a13cf106b780f0f110c0219
fierro21 <37199963+fierro21@users.noreply.github.com>
2018-05-23 23:29:14 -0700

    Update Productor.java

--- unidad3/src/pc/hilo/Productor.java
+++ unidad3/src/pc/hilo/Productor.java
@@ -1,20 +1,32 @@
 
 package pc.hilo;
 
-import java.util.Random;
+
+import java.util.Scanner;
 
 import pc.inter.Buffer;
 
 public class Productor extends Thread {
 	Buffer b;
+	int i;
 	public Productor(Buffer b) {
 		this.b = b;
 	}
 	@Override
 	public void run() {
+		
 		while(true) {
-			Random r = new Random();
-			b.setValor(r.nextInt(10)+1);
+			System.out.println("[Productor] ingrese un numero: \n");
+			Scanner sc = new Scanner(System.in);
+			b.setValor(i=sc.nextInt());
+			
+			if(i==-1) {break;}
+			try {
+				Thread.sleep(8000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
