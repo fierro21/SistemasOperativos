b0dfc41b8b8a7a43889a13b8f753f44a7e41d123
fierro21 <37199963+fierro21@users.noreply.github.com>
2018-05-23 23:28:14 -0700

    Update Consumidor.java

--- unidad3/src/pc/hilo/Consumidor.java
+++ unidad3/src/pc/hilo/Consumidor.java
@@ -3,19 +3,26 @@ package pc.hilo;
 import pc.inter.Buffer;
 
 public class Consumidor extends Thread {
-	pc.inter.Buffer b;
-	
-	public Consumidor(Buffer b){
-		this.b=b;
+	int fac,x,i;
+	Buffer b;
+	public Consumidor(Buffer b) {
+		// TODO Auto-generated constructor stub
+		this.b = b;
 	}
-	public void run(){
-		while(true){
-			System.out.println(b.getValor());
-			try {
-				Thread.sleep(1000);
-			}catch(InterruptedException e){
-				e.printStackTrace();
-			}
+	@Override
+	public void run() {
+		while(true) {
+			
+			i=b.getValor();		
+
+			fac=1;
+            for(x=1; x<=i; x++)
+         
+                  fac=x*fac;
+            
+            if (i==-1) { break;}       
+			System.out.println("[Consumidor] El factorial es "+fac);
+			 
 		}
 	}
 
